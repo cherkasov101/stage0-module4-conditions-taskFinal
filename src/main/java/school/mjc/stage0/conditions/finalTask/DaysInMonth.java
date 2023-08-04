5772b0ce69b317bc1d8020b21a117af20d214b83
Iurii Cherkasov <cherkasov1010@gmail.com>
2023-08-04 17:24:14 +0300

    m4.1 final

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,20 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (month == 2 && year > 0) {
+            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+                System.out.println(29);
+            } else {
+                System.out.println(28);
+            }
+        } else if (month >= 1 && month <= 12 && year > 0) {
+            if (month < 8 && month % 2 != 0 || month > 7 && month % 2 == 0) {
+                System.out.println(31);
+            } else {
+                System.out.println(30);
+            }
+        } else {
+            System.out.println("invalid date");
+        }
     }
 }
